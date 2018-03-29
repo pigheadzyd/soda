88bd98b274b4dec484cca70f341f32a016f2f60b
Yandong Zhu <pigheadzyd@gmail.com>
2018-03-29 12:09:22 -0400

    implement bank

--- Bank.cc
+++ Bank.cc
@@ -2,12 +2,31 @@
 
 using namespace std;
 
-Bank::Bank( unsigned int numStudents){}
+Bank::Bank( unsigned int numStudents ):
+	numStudents( numStudents ) {
+		studentBalance = new unsigned int[numStudents];
+		waiting = new uCondition[numStudents];
+		// init the basic account balance
+		for ( unsigned int i = 0; i < numStudents; ++i ) {
+			studentBalance[i] = 0;
+		}	// for
+}	// Bank::Bank
 
-void Bank::deposit( unsigned int id, unsigned int amount ){
+Bank::~Bank() {
+	delete[] studentBalance;
+	delete[] waiting;
+}	// Bank::~Bank()
 
+void Bank::deposit( unsigned int id, unsigned int amount ) {
+	studentBalance[id] += amount;
+	if ( !waiting[id].empty() ) {
+		waiting[id].signal();
+	}	// if
 }
 
 void Bank::withdraw( unsigned int id, unsigned int amount ){
-	
+	while ( studentBalance[id] < amount ) {
+		waiting[id].wait();
+	}	// while
+	studentBalance[id] -= amount;
 }
--- Bank.h
+++ Bank.h
@@ -4,8 +4,13 @@
 _Monitor Bank {
   public:
     Bank( unsigned int numStudents );
+    ~Bank();
     void deposit( unsigned int id, unsigned int amount );
     void withdraw( unsigned int id, unsigned int amount );
+  private:
+  	unsigned int numStudents;
+  	unsigned int* studentBalance;
+  	uCondition * waiting;
 };
 
 #endif
