d6a7a528179bb848ef725a69b8f7251620ade84f
Yandong Zhu <pigheadzyd@gmail.com>
2018-03-30 00:24:14 -0400

    implement bottling plant

--- BottlingPlant.cc
+++ BottlingPlant.cc
@@ -1,3 +1,5 @@
+#include <algorthim>
+
 #include "BottlingPlant.h"
 #include "Printer.h"
 #include "NameServer.h"
@@ -19,17 +21,38 @@ BottlingPlant::BottlingPlant(
 	timeBetweenShipments( timeBetweenShipments ),
 	shutdown( false ){}
 
+BottlingPlant::~BottlingPlant() {}
 
 void BottlingPlant::main() {
 	prt.print( Printer::BottlingPlant, 'S' );
 
 	for ( ;; ) {
-		
+		_Accept( ~BottlingPlant ) {
+			shutdown = true;
+			break;
+		} _Else {
+			int total = 0;
+			for ( int i = 0; i < SODA_FLAVOUR; ++i ) {
+				product[i] = mprng( maxShippedPerFlavour );
+				total += product[i];
+			}	// for
+			prt.print( Printer::BottlingPlant, 'G', total );
+
+			yield( timeBetweenShipments );
+
+			_Accept( getShipment );
+			prt.print( Printer::BottlingPlant, 'P' );
+			// for ( int i = 0; i < SODA_FLAVOUR; ++i ) {
+			// 	product[i] = 0;
+			// }	// reset the value
+		}	// _Accept
 	}	// for
+	_Accept ( getShipment );
 }
 
 void getShipment( unsigned int cargo[] ){
 	if ( shutdown ) {
 		_Throw shutdown();
-	}
+	}	// if
+	std::copy( product, product + SODA_FLAVOUR, cargo );
 }
--- BottlingPlant.h
+++ BottlingPlant.h
@@ -9,16 +9,18 @@ _Task BottlingPlant {
     BottlingPlant( Printer & prt, NameServer & nameServer, unsigned int numVendingMachines,
                  unsigned int maxShippedPerFlavour, unsigned int maxStockPerFlavour,
                  unsigned int timeBetweenShipments );
+    ~BottlingPlant();
     void getShipment( unsigned int cargo[] );
   private:
   	Printer & prt;
   	NameServer & nameServer;
+    int SODA_FLAVOUR = 4;
   	unsigned int numVendingMachines;
   	unsigned int maxShippedPerFlavour;
   	unsigned int maxStockPerFlavour;
   	unsigned int timeBetweenShipments;
+    unsigned int product[SODA_FLAVOUR];
   	bool shutdown;
-  	bool take;
     void main();
 };
 
