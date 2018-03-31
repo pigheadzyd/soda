825a75656177d0c4ad1f657d7137e1fcc6c062a9
Yandong Zhu <pigheadzyd@gmail.com>
2018-03-30 20:29:34 -0400

    first draft for truck

--- Truck.cc
+++ Truck.cc
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <algorithm>
 
 #include "MPRNG.h"
 #include "Truck.h"
 #include "Printer.h"
 #include "NameServer.h"
 #include "BottlingPlant.h"
+#include "VendingMachine.h"
 
 extern MPRNG mprng;
 
@@ -17,12 +19,46 @@ Truck::Truck(
 	prt( prt ), nameServer( nameServer ), plant( plant ), 
 	numVendingMachines ( numVendingMachines ), 
 	maxStockPerFlavour ( maxStockPerFlavour ) {
-		std::cout << "create truck" << std::endl;
-	}
+		for ( int i = 0; i < SODA_FLAVOUR; ++i ) {
+			emptyStatus[i] = false;
+		}
+}
 
-void Truck:: main() {
+void Truck::main() {
 	prt.print( Printer::Truck, 's' );
 	for ( ;; ) {
-		yield( mprng( 10 ) );
-	}	// 
+		yield( mprng( 1, 10 ) );
+		try {
+			plant.getShipment( product );
+		} catch ( BottlingPlant::Shutdown & ) {
+			break;
+		}	// try
+		VendingMachine ** vendingMachineList =  nameServer.getMachineList();
+		for ( int i = 0; i < numVendingMachines; ++i ) {
+			if ( !emptyStatus[i] ) {
+				unsigned int * curStock = vendingMachineList[curIndex]->inventory();
+				for ( int i = 0; i < SODA_FLAVOUR; ++i ) {
+					unsigned int rest = curStock[i];
+					unsigned int restockNum = std::min( maxStockPerFlavour - rest, product[i] );
+					product[i] -= restockNum;
+					curStock[i] += restockNum;
+					// if the current flavour is empty
+					if ( product[i] == 0 ) {
+						empty++;
+						emptyStatus[i] = true;
+					}	// if
+				}	// for
+			}	// if
+			vendingMachineList[curIndex]->restock();
+			curIndex = ( curIndex + 1 ) % numVendingMachines;
+			// if a product in the truck is empty
+			if ( empty == SODA_FLAVOUR ) {
+				empty = 0;
+				for ( int i = 0; i < SODA_FLAVOUR; ++i ) {
+					emptyStatus = false;
+				}	// for
+				break;
+			} // if
+		}	// for
+	}	// for
 }
--- Truck.h
+++ Truck.h
@@ -11,8 +11,14 @@ _Task Truck {
   	Printer & prt;
   	NameServer & nameServer;
   	BottlingPlant & plant;
+    int currentIndex = 0;
+    int empty = 0;
+    int SODA_FLAVOUR = 4;
+    unsigned int currentIndex;
   	unsigned int numVendingMachines;
   	unsigned int maxStockPerFlavour;
+    unsigned int product[SODA_FLAVOUR];
+    bool emptyStatus[SODA_FLAVOUR];
     void main();
 };
 
