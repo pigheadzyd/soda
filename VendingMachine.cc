3c87fba4c3bdde280f63002baabfc85ef22c50a1
Yandong Zhu <pigheadzyd@gmail.com>
2018-04-03 17:29:57 -0400

    vending without buy

--- VendingMachine.cc
+++ VendingMachine.cc
@@ -1,20 +1,86 @@
 #include "VendingMachine.h"
+#include "Printer.h"
+#include "NameServer.h"
+#include "WATCard.h"
+#include "MPRNG.h"
 
-VendingMachine::VendingMachine( Printer & prt, NameServer & nameServer, unsigned int id, unsigned int sodaCost,
-                    unsigned int maxStockPerFlavour );
+VendingMachine::VendingMachine( 
+	Printer & prt, 
+	NameServer & nameServer, 
+	unsigned int id, 
+	unsigned int sodaCost,
+	unsigned int maxStockPerFlavour ):
+	prt( prt ), nameServer( nameServer ),
+	id ( id ), sodaCost( sodaCost ), 
+	maxStockPerFlavour( maxStockPerFlavour ) {
+		curStock = new unsigned int[SODA_FLAVOUR];
+		for ( int i = 0; i < SODA_FLAVOUR; ++i ) {	// init the value
+			curStock[i] = 0;
+		}	// for
+		flavourMap[lasmoi0] = 0;
+		flavourMap[lasmoi1] = 1;
+		flavourMap[lasmoi2] = 2;
+		flavourMap[lasmoi3] = 3;
+}
 
-void VendingMachine::buy( Flavours flavour, WATCard & card ){}
+VendingMachine::~VendingMachine() {
+
+	delete[] curStock;
+}
+
+unsigned int VendingMachine::getFlavourID( Flavours flavour ) {
+	return flavourMap.at( flavour );
+}
+
+void VendingMachine::buy( Flavours flavour, WATCard & card ) {
+	if ( !buyFlag ) {
+		restockLock.wait();
+	}	// if
+	// check enough balance
+	if ( card.getBalance() < sodaCost ) {
+		_Throw Fund();
+	}	// if
+	// check enough flavour
+	int flavourIndex = getFlavourID( flavour );
+	if ( curStock[flavourIndex] == 0 ) {
+		_Throw Stock();
+	}	// if 
+
+	unsigned int free = mprng( 4 );
+	if ( !free ) {
+		buyLock.signal();
+		mainLock.wait();
+	}	// if
+
+}
+
+void VendingMachine::main() {
+	for ( ;; ) {
+		_Accept( ~VendingMachine ) {
+			break;
+		}_Else {
+
+		}
+	}
+}
 
 unsigned int * VendingMachine::inventory(){
-	return 0;
+	buyFlag = false;
+	return curStock;
 }          
 
-void VendingMachine::restocked() {}
+void VendingMachine::restocked() {
+	// wake up the 
+	while ( !buyLock.empty() ) {
+		buyLock.signal();
+	}	// while
+	buyFlag = true;
+}
 
 _Nomutex unsigned int VendingMachine::cost(){
-	return 0;
+	return sodaCost;
 }  
 
 _Nomutex unsigned int VendingMachine::getId(){
-	return 0;
+	return id;
 }
--- VendingMachine.h
+++ VendingMachine.h
@@ -1,20 +1,41 @@
 #ifndef __VENDINGMACHINE_H__
 #define __VENDINGMACHINE_H__
+#include <map>
+class Printer;
+class NameServer;
+class WATCard;
 
 _Task VendingMachine {
-    void main();
   public:
-    enum Flavours { ... };                    // flavours of soda (YOU DEFINE)
+    enum Flavours { lasmoi0, lasmoi1, lasmoi2, lasmoi3 };                    // flavours of soda (YOU DEFINE)
     _Event Free {};                           // free, advertisement
     _Event Funds {};                          // insufficient funds
     _Event Stock {};                          // out of stock for particular flavour
     VendingMachine( Printer & prt, NameServer & nameServer, unsigned int id, unsigned int sodaCost,
                     unsigned int maxStockPerFlavour );
+    ~VendingMachine();
     void buy( Flavours flavour, WATCard & card );
     unsigned int * inventory();
     void restocked();
     _Nomutex unsigned int cost();
     _Nomutex unsigned int getId();
+  private:
+    Printer & prt;
+    NameServer & nameServer;
+    int SODA_FLAVOUR = 4;
+    unsigned int id;
+    unsigned int soda;
+    unsigned int sodaCost;
+    unsigned int maxStockPerFlavour;
+    unsigned int * curStock;
+    bool buyFlag = true;
+    uCondition buyLock;
+    uCondition mainLock;
+    uCondition restockLock;
+    std::map<Flavours, unsigned int> flavourMap;
+    unsigned int getFlavourId( Flavours flavour );
+    void main();
+
 };
 
 #endif
