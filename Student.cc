2a9d42d13d3d79965065c44f227b6757cc29ea0a
Yandong Zhu <pigheadzyd@gmail.com>
2018-04-04 16:49:26 -0400

    student documentation

--- Student.cc
+++ Student.cc
@@ -29,68 +29,78 @@ Student::Student(
   favouriteFlavourInt = mprng( 3 );                 // Determine the favourite flavour
   favouriteFlavour = ( VendingMachine::Flavours ) favouriteFlavourInt;  // Four favourite flavours
 
-}
+} // Student::Student
 
 Student::~Student() {
   prt.print( Printer::Kind::Student, id, 'F');
-}
+} // Student::~Student
 
-void  Student::main() {
+
+//--------------------------------------------------------------------------------------------------------------------
+// Handle the student to buy the amount of soda with their favourite flavour.
+// With the situation using giftcard or WATCard to buy
+//--------------------------------------------------------------------------------------------------------------------
+void Student::main() {
 
   prt.print( Printer::Kind::Student, id, 'S', favouriteFlavourInt, numOfPurchases );
-  watcard = cardOffice.create( id, 5 );
-  groupOffCard = groupoff.giftCard();
-  vendingMachine = nameServer.getMachine( id );
-  prt.print( Printer::Kind::Student, id, 'V', vendingMachine->getId());
+  watcard = cardOffice.create( id, 5 );                  // Create a new WATCard
+  groupOffCard = groupoff.giftCard();                    // Require for a giftcard
+
+  vendingMachine = nameServer.getMachine( id );     
+  prt.print( Printer::Kind::Student, id, 'V', vendingMachine->getId() );
+
   for( unsigned int i = 0; i < numOfPurchases; ) {
-    _Select( groupOffCard ) {
+    _Select( groupOffCard ) {                             // Case of using giftcard to purchase???
       try {
-        curCard = groupOffCard();                         // all gift cards are deleted by group off at last
-        yield(mprng(1,10));
+        curCard = groupOffCard();                         // Set the purchase cardtype
+        yield( mprng( 1,10 ) );
+
         vendingMachine->buy( favouriteFlavour, *curCard );
         prt.print( Printer::Kind::Student, id, 'G', favouriteFlavourInt, curCard->getBalance() );
-        i += 1;
+
+        i += 1;                                           // Successfully purchase without special case
         groupOffCard.reset();
         curCard = NULL;
-      } catch ( VendingMachine::Free freeEvent ) {
+      } catch ( VendingMachine::Free freeEvent ) {        // If this purchase is free
         prt.print( Printer::Kind::Student, id, 'a', favouriteFlavourInt, curCard->getBalance() );
-        yield(4);
-        i += 1;                                           // +1 because one consumed
-      } catch ( VendingMachine::Stock stockEvent ) {
-        vendingMachine = nameServer.getMachine( id );     // get the new machine
-        prt.print( Printer::Kind::Student, id, 'V', vendingMachine->getId());
-      }
+        yield( 4 );
+        i += 1;                                           
+      } catch ( VendingMachine::Stock stockEvent ) {      // If this flavour in this vending machine is out of stock
+        vendingMachine = nameServer.getMachine( id );     // Get the new machine
+        prt.print( Printer::Kind::Student, id, 'V', vendingMachine->getId() );
+      } // try
+      // ??? if no bug then delete (next line)
       // if ( curGiftCard->getBalance() == 0 ) groupOffCard.reset();
-    } or _Select( watcard ) {
+    } or _Select( watcard ) {                             // Case of using WATCard to purchase??? 
       try {
-        curCard = watcard();                              // this card should be deleted in gift card class?
-        yield( mprng(1, 10 ) );
+        curCard = watcard();                              // Set the purchase cardtype
+        yield( mprng( 1, 10 ) );
         vendingMachine->buy( favouriteFlavour, *curCard );
         prt.print( Printer::Kind::Student, id, 'B', favouriteFlavourInt, curCard->getBalance() );
         i += 1;
 
-      } catch ( WATCardOffice::Lost lostEvent ) {
-        prt.print( Printer::Kind::Student, id, 'L');
-        watcard = cardOffice.create( id, 5 );
+      } catch ( WATCardOffice::Lost lostEvent ) {         // If the WATCarad is lost
+        prt.print( Printer::Kind::Student, id, 'L' );
+        watcard = cardOffice.create( id, 5 );             // Get a new one
 
-      } catch ( VendingMachine::Free freeEvent ) {
+      } catch ( VendingMachine::Free freeEvent ) {        // If this purchase is free
         prt.print( Printer::Kind::Student, id, 'A', favouriteFlavourInt, curCard->getBalance() );
         yield(4);
         i += 1;  
-      } catch ( VendingMachine::Stock stockEvent ) {
-        vendingMachine = nameServer.getMachine( id );     // get the new machine
-        prt.print( Printer::Kind::Student, id, 'V', vendingMachine->getId());
+      } catch ( VendingMachine::Stock stockEvent ) {      // If this flavour in this vending machine is out of stock
+        vendingMachine = nameServer.getMachine( id );     // Get the new machine
+        prt.print( Printer::Kind::Student, id, 'V', vendingMachine->getId() );
 
-      } catch ( VendingMachine::Funds fundEvent ) {       // transfer
+      } catch ( VendingMachine::Funds fundEvent ) {       // If there is not enough money in this WATCard
         watcard = cardOffice.transfer( id, 5 + vendingMachine->cost(), curCard );
-
       } // try
-    } // select
+    } // _Select
 
   } // for
-  try {                           // handle the case that Gifcard purchases the last bottle
+  try {                                                   // Handle the case that Gifcard purchases the last bottle
     curCard = watcard();
     delete curCard;
   } catch ( WATCardOffice::Lost lostEvent ) {
-  }
-} // main
+  } // try
+
+} // Student::main
