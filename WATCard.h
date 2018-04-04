df0755e5d2c08c2ea541916ade5ea8fbebe1ffc3
Yandong Zhu <pigheadzyd@gmail.com>
2018-04-04 17:22:27 -0400

    watcard document

--- WATCard.h
+++ WATCard.h
@@ -1,3 +1,6 @@
+//--------------------------------------------------------------------------------------------------------------------
+// Define the interface to the WATCard class for the vending machine system.
+//--------------------------------------------------------------------------------------------------------------------
 #ifndef __WATCARD_H__
 #define __WATCARD_H__
 #include <uFuture.h>
@@ -6,11 +9,11 @@ class WATCard {
   public:
     typedef Future_ISM<WATCard *> FWATCard;   // future watcard pointer
     WATCard();
-    void deposit( unsigned int amount );
-    void withdraw( unsigned int amount );
-    unsigned int getBalance();
+    void deposit( unsigned int amount );      // Deposit the money into WATCard
+    void withdraw( unsigned int amount );     // Withdraw the money from WATCard
+    unsigned int getBalance();                // Get the current balance
   private:
-  	unsigned int balance = 0;
+  	unsigned int balance;                     // The balance for a WATCard
     WATCard( const WATCard & ) = delete;      // prevent copying
     WATCard & operator=( const WATCard & ) = delete;
 };
