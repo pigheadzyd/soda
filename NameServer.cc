3e24f130942c04ea1e0a392d09dd841c096034f6
Yandong Zhu <pigheadzyd@gmail.com>
2018-04-04 15:52:09 -0400

    nameserver documentation

--- NameServer.cc
+++ NameServer.cc
@@ -1,63 +1,79 @@
-#include "NameServer.h"
+#include "NameServer.h"																	// Definition of NameServer Monitor
 #include "VendingMachine.h"
 #include "Printer.h"
 
 
+//--------------------------------------------------------------------------------------------------------------------
+// Starup and shutdown.
+//--------------------------------------------------------------------------------------------------------------------
 NameServer::NameServer( 
 	Printer & prt, 
 	unsigned int numVendingMachines, 
 	unsigned int numStudents ): 
 	prt( prt ), 
-	// curIndex( 0 ), curRegister( 0 ),
 	numVendingMachines( numVendingMachines ), 
 	numStudents( numStudents ) {
 		vendingMachineList = new VendingMachine *[numVendingMachines];
 		machineOfStudent = new unsigned int[numStudents];
-		for ( unsigned int i = 0; i < numStudents; i++ ) {
+		for ( unsigned int i = 0; i < numStudents; i++ ) {		// Assign the default machine for student
 			machineOfStudent[i] = i % numVendingMachines; 
-		}
+		}	// for
 }
 
 NameServer::~NameServer() {
 	delete[] vendingMachineList;
 	delete[] machineOfStudent;
+	prt.print( Printer::Kind::NameServer, 'F' );
 }
 
-void NameServer::VMregister ( VendingMachine * vendingmachine ) {
+
+//--------------------------------------------------------------------------------------------------------------------
+// Register the vending machine into nameserver by there id.
+//--------------------------------------------------------------------------------------------------------------------
+void NameServer::VMregister( VendingMachine * vendingmachine ) {
 	vendingMachineList[vendingmachine->getId()] = vendingmachine;
 	prt.print( Printer::Kind::NameServer, 'R', vendingmachine->getId() );
-}
+}	// NameServer::VMregister
 
+
+//--------------------------------------------------------------------------------------------------------------------
+// Student ask to find a vending machine to buy soda.
+//--------------------------------------------------------------------------------------------------------------------
 VendingMachine * NameServer::getMachine( unsigned int id ) {
 	int index = machineOfStudent[id];
 	prt.print( Printer::Kind::NameServer, 'N', id, index );
-	machineOfStudent[id] = ( index + 1 ) % numVendingMachines;
+	machineOfStudent[id] = ( index + 1 ) % numVendingMachines;	// Determine the next vending machine when the student call next time
 	return vendingMachineList[index];
 }
 
+
+//--------------------------------------------------------------------------------------------------------------------
+// Register the vending machine into nameserver by there id.
+//--------------------------------------------------------------------------------------------------------------------
 VendingMachine ** NameServer::getMachineList() {
 	return vendingMachineList;
-}	// VendingMachine ** NameServer::getMachineList
+}	// NameServer::getMachineList
 
 
+//--------------------------------------------------------------------------------------------------------------------
+// Register the vending machine into nameserver by there id.
+//--------------------------------------------------------------------------------------------------------------------
 void NameServer::main() {
-	// print start
 	prt.print( Printer::Kind::NameServer, 'S' );
 
-	for ( unsigned int i = 0; i < numVendingMachines; ){
+	for ( unsigned int i = 0; i < numVendingMachines; ){				// Waiting for all vending machine have registered
 		_Accept( VMregister ) {
 			i++;
-		}
+		}	// _Accept
 	} // for
 
 	for ( ;; ) {
-		_Accept( ~NameServer ) {
+		_Accept( ~NameServer ) {																	// Accept the destuctor to break the loop
 			break;
-		} or _Accept( getMachine ) {
+		} or _Accept( getMachine ) {															// Accept the getMachine case
 
-		} or _Accept( getMachineList ) {
+		} or _Accept( getMachineList ) {													// Accept the getMachineList case
 
-		}
+		}	// _Accept
 	} // for
-	prt.print( Printer::Kind::NameServer, 'F' );
 }
--- NameServer.h
+++ NameServer.h
@@ -1,3 +1,6 @@
+//--------------------------------------------------------------------------------------------------------------------
+// Define the interface to the NameServer monitor for the vending machine system.
+//--------------------------------------------------------------------------------------------------------------------
 #ifndef __NAMESERVER_H__
 #define __NAMESERVER_H__
 
@@ -8,18 +11,16 @@ _Task NameServer {
   public:
     NameServer( Printer & prt, unsigned int numVendingMachines, unsigned int numStudents );
     ~NameServer();
-    void VMregister( VendingMachine * vendingmachine );
-    VendingMachine * getMachine( unsigned int id );
-    VendingMachine ** getMachineList();
+    void VMregister( VendingMachine * vendingmachine ); // Vending matchine to register themself
+    VendingMachine * getMachine( unsigned int id );     // Student ask for a vending machine
+    VendingMachine ** getMachineList();                 // Get the whole list of the registered vending machine
   private:
   	Printer & prt;
-  	// int curIndex;
-  	// int curRegister;
-  	unsigned int numVendingMachines;                 	// number of total machines
-  	unsigned int numStudents;                        	// number of total students
-  	VendingMachine ** vendingMachineList;            	// map machine id to machine pointer
-    unsigned int *machineOfStudent;                		// map student to machine id which is managed saperately
-  	void main();
+  	unsigned int numVendingMachines;                 	 // Total number of vending machines
+  	unsigned int numStudents;                        	 // Total number of students
+  	VendingMachine ** vendingMachineList;            	 // Map machine id to machine pointer
+    unsigned int *machineOfStudent;                		 // Map student to machine id which is managed saperately
+  	void main();                                       // Main body of task
 };
 
 #endif
