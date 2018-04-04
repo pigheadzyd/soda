dd0302dcc25ad6a2d0500c83a226abc89f192377
Yandong Zhu <pigheadzyd@gmail.com>
2018-04-04 14:25:06 -0400

    Adding documentation for bank

--- Bank.cc
+++ Bank.cc
@@ -1,13 +1,16 @@
-#include "Bank.h"
+#include "Bank.h"																					// Definition of Bank monitor
 
 using namespace std;
 
+
+//--------------------------------------------------------------------------------------------------------------------
+// Starup and shutdown.
+//--------------------------------------------------------------------------------------------------------------------
 Bank::Bank( unsigned int numStudents ):
 	numStudents( numStudents ) {
 		studentBalance = new unsigned int[numStudents];
 		waiting = new uCondition[numStudents];
-		// init the basic account balance
-		for ( unsigned int i = 0; i < numStudents; ++i ) {
+		for ( unsigned int i = 0; i < numStudents; ++i ) {		// Init the basic account balance
 			studentBalance[i] = 0;
 		}	// for
 }	// Bank::Bank
@@ -15,18 +18,26 @@ Bank::Bank( unsigned int numStudents ):
 Bank::~Bank() {
 	delete[] studentBalance;
 	delete[] waiting;
-}	// Bank::~Bank()
+}	// Bank::~Bank
 
+
+//--------------------------------------------------------------------------------------------------------------------
+// Deposit the amount money into the id student 
+//--------------------------------------------------------------------------------------------------------------------
 void Bank::deposit( unsigned int id, unsigned int amount ) {
 	studentBalance[id] += amount;
-	if ( !waiting[id].empty() ) {
+	if ( !waiting[id].empty() ) {														// If withdraw are waiting
 		waiting[id].signal();
 	}	// if
-}
+}	// Bank::deposit
 
+
+//--------------------------------------------------------------------------------------------------------------------
+// Withdraw the amount money from the id student 
+//--------------------------------------------------------------------------------------------------------------------
 void Bank::withdraw( unsigned int id, unsigned int amount ){
-	while ( studentBalance[id] < amount ) {
+	while ( studentBalance[id] < amount ) {									// Waiting until get the balance has enough money
 		waiting[id].wait();
 	}	// while
 	studentBalance[id] -= amount;
-}
+}	// Bank::withdraw
--- Bank.h
+++ Bank.h
@@ -1,3 +1,7 @@
+//--------------------------------------------------------------------------------------------------------------------
+// Define the interface to the bank monitor for the vending machine system.
+//--------------------------------------------------------------------------------------------------------------------
+
 #ifndef __BANK_H__
 #define __BANK_H__
 
@@ -5,12 +9,12 @@ _Monitor Bank {
   public:
 		Bank( unsigned int numStudents );
 		~Bank();
-		void deposit( unsigned int id, unsigned int amount );
-		void withdraw( unsigned int id, unsigned int amount );
+		void deposit( unsigned int id, unsigned int amount );			// deposit the amount of money into student with id
+		void withdraw( unsigned int id, unsigned int amount );		// withdraw the amount of money from student with id
   private:
-  	unsigned int numStudents;
-  	unsigned int* studentBalance;
-  	uCondition * waiting;
+  	unsigned int numStudents;																	// the number of student 
+  	unsigned int * studentBalance;														// the array of current students' balance
+  	uCondition * waiting;																			// the condition array for waiting if there is not enough money for withdraw
 };
 
 #endif
