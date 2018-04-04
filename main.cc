2a0b0f49c3867057a6d761f33aafc0b936735959
lasmoi <lasmoihu33@gmail.com>
2018-04-04 00:45:01 -0400

    office blocking

--- main.cc
+++ main.cc
@@ -52,21 +52,21 @@ int main(int argc, char const *argv[]){
 
 	Printer printer( configParms.numStudents, configParms.numVendingMachines, configParms.numCouriers );
 	Bank bank ( configParms.numStudents );
-	Parent * parent = new Parent( printer, bank, configParms.numStudents, configParms.parentalDelay );
+	Parent parent( printer, bank, configParms.numStudents, configParms.parentalDelay );
 	Groupoff groupoff( printer, configParms.numStudents, configParms.sodaCost, configParms.groupoffDelay );
-	WATCardOffice * office = new WATCardOffice( printer, bank, configParms.numCouriers );
-	NameServer * nameServer = new NameServer( printer, configParms.numVendingMachines, configParms.numStudents );
-	BottlingPlant * bottlingPlant = new BottlingPlant( printer, *nameServer, configParms.numVendingMachines, 
+	WATCardOffice office( printer, bank, configParms.numCouriers );
+	NameServer nameServer( printer, configParms.numVendingMachines, configParms.numStudents );
+	BottlingPlant * bottlingPlant = new BottlingPlant( printer, nameServer, configParms.numVendingMachines, 
 		configParms.maxShippedPerFlavour, configParms.maxStockPerFlavour, configParms.timeBetweenShipments );
 
 	Student * studentList[configParms.numStudents];
 	for ( unsigned int i = 0; i < configParms.numStudents; ++i ) {
-		studentList[i] = new Student( printer, *nameServer, *office, groupoff, i, configParms.maxPurchases );
+		studentList[i] = new Student( printer, nameServer, office, groupoff, i, configParms.maxPurchases );
 	} // for
 
 	VendingMachine * vendingMachineList[configParms.numVendingMachines];
 	for ( unsigned int i = 0; i < configParms.numVendingMachines; ++i ) {
-		vendingMachineList[i] = new VendingMachine( printer, *nameServer, i, configParms.sodaCost, configParms.maxStockPerFlavour );
+		vendingMachineList[i] = new VendingMachine( printer, nameServer, i, configParms.sodaCost, configParms.maxStockPerFlavour );
 	} // for
 
 
@@ -80,12 +80,6 @@ int main(int argc, char const *argv[]){
 	for ( unsigned int i = 0; i < configParms.numVendingMachines; ++i ) {
 		delete vendingMachineList[i];
 	} // for		
-	cout << "parent deleting" << endl;
-	delete parent;
-	cout << "office deleting" << endl;
-	delete office;
-	cout << "nameServer deleting" << endl;
-	delete nameServer;
-	cout << "returning" << endl;
+cout << "returning deleting" << endl;
 	return 0;
 }
