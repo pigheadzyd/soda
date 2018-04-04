5f4af23af3891ee0adf4c6b9730c96f513adf5bf
Yandong Zhu <pigheadzyd@gmail.com>
2018-04-04 17:54:46 -0400

    truck document

--- Truck.cc
+++ Truck.cc
@@ -1,17 +1,17 @@
-#include <iostream>
-#include <algorithm>
-#include <numeric>
-#include <string>
+#include <algorithm>						// std::min
+#include <numeric>							// std::accumulate
 
 #include "MPRNG.h"
-#include "Truck.h"
+#include "Truck.h"							// Definition of Truck Task
 #include "Printer.h"
 #include "NameServer.h"
 #include "BottlingPlant.h"
 #include "VendingMachine.h"
 
-using namespace std;
 
+//--------------------------------------------------------------------------------------------------------------------
+// Starup and shutdown.
+//--------------------------------------------------------------------------------------------------------------------
 Truck::Truck( 
 	Printer & prt, 
 	NameServer & nameServer, 
@@ -21,107 +21,86 @@ Truck::Truck(
 	prt( prt ), nameServer( nameServer ), plant( plant ), 
 	numVendingMachines ( numVendingMachines ), 
 	maxStockPerFlavour ( maxStockPerFlavour ),
-	SODA_FLAVOUR( 4 ) {
+	curIndex( 0 ), total( 0 ), SODA_FLAVOUR( 4 ) {
 		product = new unsigned int[SODA_FLAVOUR];
 		emptyStatus = new bool[SODA_FLAVOUR];
 
 		for ( int i = 0; i < SODA_FLAVOUR; ++i ) {
 			emptyStatus[i] = false;
 		}	// for
-}
+}	// Truck::Truck
 
 Truck::~Truck() {
 	delete[] product;
 	delete[] emptyStatus;
-}
-
-void Truck::print( string kind, int value1, int value2, int value3, int value4 ) {
-	cout << '\t' << '\t' << '\t' << '\t' ;
-	cout << kind << value1 << ',' << value2 << ',' << value3 << ',' << value4 << endl;
-}
+	prt.print( Printer::Truck, 'F' );
+}	// Truck::~Truck
 
 
-void Truck::print( string kind, unsigned int id, int value1, int value2, int value3, int value4 ) {
-	cout << '\t' << '\t' << '\t' << '\t' ;
-	cout << kind << ' ' << id << ' ' << value1 << ',' << value2 << ',' << value3 << ',' << value4 << endl;
-}
-
+//--------------------------------------------------------------------------------------------------------------------
+// Truck collect product from bottling plant and deliver to vending machine.
+//--------------------------------------------------------------------------------------------------------------------
 void Truck::main() {
-	// start truck
 	prt.print( Printer::Truck, 'S' );
+
 	for ( ;; ) {
 		yield( mprng( 1, 10 ) );
-		// pick up shipment
-		try {
+		
+		try {																													// Pick up shipment
 			plant.getShipment( product );
 			total = std::accumulate( product, product + SODA_FLAVOUR, 0 );
-		} catch ( BottlingPlant::Shutdown & ) {
+		} catch ( BottlingPlant::Shutdown & ) {												// If the bottling plant is shutdown
 			break;
 		}	// try
+
 		prt.print( Printer::Truck, 'P', total );
-		//print( "product", product[0], product[1], product[2], product[3] );
-		// get teh vending machine
+	
 		VendingMachine ** vendingMachineList =  nameServer.getMachineList();
 
-		for ( unsigned int i = 0; i < numVendingMachines; ++i ) {			// for loop all the vending machines
+		for ( unsigned int i = 0; i < numVendingMachines; ++i ) {			// Loop all the vending machines
 
-			// get the vending machine and calculte the replenished request
 			prt.print( Printer::Truck, 'd', curIndex, total );
-
 			unsigned int * curStock = vendingMachineList[curIndex]->inventory();
 
-			//print( "Origin", curIndex, curStock[0], curStock[1], curStock[2], curStock[3] );
+			// Get the vending machine and calculte the replenished request
 			maxTotal = SODA_FLAVOUR * maxStockPerFlavour - std::accumulate( curStock, curStock + SODA_FLAVOUR, 0 );
 
-			for ( int j = 0; j < SODA_FLAVOUR; ++j ) {				// install all the flavour into the vending machine
-				if ( !emptyStatus[j] ) {												// if the current flavour is not empty yet
+			for ( int j = 0; j < SODA_FLAVOUR; ++j ) {								// Install all the flavour into the vending machine
+				if ( !emptyStatus[j] ) {																// If the current flavour is not empty yet
 					unsigned int rest = curStock[j];
-					//cout << "flavour " << j << " stock " << curStock[j] << endl;
-					//cout << "remain " << maxStockPerFlavour - rest << endl;
-					//cout << "flavour " << j << " product " << product[j] << endl;
 					unsigned int restockNum = std::min( maxStockPerFlavour - rest, product[j] );
-					//cout << "restock " << restockNum << endl;
-					// restore this kind flavour
-					product[j] -= restockNum;
-					curStock[j] += restockNum;
-					// calculate the total number
-					maxTotal = maxTotal - restockNum;
+					product[j] -= restockNum;															// Restore this kind flavour
+					curStock[j] += restockNum;														
+					maxTotal = maxTotal - restockNum;											// Calculate the total number
 					total = total - restockNum;
 				}	// if
 
-				// if the current flavour is empty
-				if ( product[j] == 0 ){
+				if ( product[j] == 0 ){																	// If the current flavour is empty
 					emptyStatus[j] = true;
 				}	// if
 			}	// for
 
-			// told the vending machine finish restock
-			vendingMachineList[curIndex]->restocked();
+			vendingMachineList[curIndex]->restocked();								// Notify the vending machine finish restock
 
-			//print( "After", curIndex, curStock[0], curStock[1], curStock[2], curStock[3] );
-			//print( "T after", product[0], product[1], product[2], product[3] );
-			//	if it is not filled the vending machine
-			if ( maxTotal > 0 ){
+			if ( maxTotal > 0 ){																			// If it is not filled the vending machine 
 				prt.print( Printer::Truck, 'U', vendingMachineList[curIndex]->getId(), maxTotal );
 			}	// if
 			
-			// if a product in the truck is empty
-			if ( total == 0 ) {
+			if ( total == 0 ) {																				// If truck is empty
 				break;
 			}	// if
 
-			curIndex = ( curIndex + 1 ) % numVendingMachines;
+			curIndex = ( curIndex + 1 ) % numVendingMachines;					// Move to next vending machine
 		}	// for
 
 		prt.print( Printer::Truck, 'D', curIndex, total );
-		//print( "Final", product[0], product[1], product[2], product[3] );
-		// reset the value
-		for ( int j = 0; j < SODA_FLAVOUR; ++j ) {
+		
+		for ( int j = 0; j < SODA_FLAVOUR; ++j ) {									// Reset the value
 			emptyStatus[j] = false;
 		}	// for
-		if ( total == 0 ) {
+
+		if ( total == 0 ) {																					// If it does not loop all the vending machine
 			curIndex = ( curIndex + 1 ) % numVendingMachines;
 		}	// if
 	}	// for
-	prt.print( Printer::Truck, 'F' );
-}
+}	// Truck::main
--- Truck.h
+++ Truck.h
@@ -1,6 +1,9 @@
+//--------------------------------------------------------------------------------------------------------------------
+// Define the interface to the Truck Task for the vending machine system.
+//--------------------------------------------------------------------------------------------------------------------
 #ifndef __TRUCK_H__
 #define __TRUCK_H__
-#include <string>
+
 _Monitor Printer;
 _Task NameServer;
 _Task BottlingPlant;
@@ -11,20 +14,18 @@ _Task Truck {
            unsigned int numVendingMachines, unsigned int maxStockPerFlavour );
     ~Truck();
   private:
-  	Printer & prt;
-  	NameServer & nameServer;
-  	BottlingPlant & plant;
-    int curIndex = 0;
-    unsigned int total = 0;
-    unsigned int maxTotal;
-  	unsigned int numVendingMachines;
-  	unsigned int maxStockPerFlavour;
-    int SODA_FLAVOUR;
-    unsigned int * product;
-    bool * emptyStatus;
-    void main();
-    void print( std::string kind, int value1, int value2, int value3, int value4 );
-    void print( std::string kind, unsigned int id, int value1, int value2, int value3, int value4 );
+  	Printer & prt;                     // Current printer
+  	NameServer & nameServer;           // Current nameserver
+  	BottlingPlant & plant;             // Current bottling plant
+  	unsigned int numVendingMachines;   // Total number of vending machine
+  	unsigned int maxStockPerFlavour;   // Max number
+    int curIndex;                      // Next vending machine suspose to restock
+    unsigned int total;                // Toal number of soda get from bottling plant
+    int SODA_FLAVOUR;                  // Total kind of flavour
+    unsigned int maxTotal;             // Max number of a ventding machine require to restock 
+    unsigned int * product;            // Array of the number of flavour get from the bottling plant
+    bool * emptyStatus;                // Array to indicating the flavour is empty of not
+    void main();                       // Main body of task
 };
 
 #endif
